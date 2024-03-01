fb8a0c1a12e84cac62c4c9168efab6821a857059
unknown <sangaypenjor289@gmail.com>
2024-03-01 23:07:57 +0530

    class exersice

---
+++ exersice.py
@@ -0,0 +1,9 @@
+x=int(input("Enter your age : "))
+input("Are youa student (yes or no):")
+
+if x==12 and 13<=x>=18:
+    print("you are eligible for the disscount")
+
+elif x>18:
+    print("you are not eligible for the discount")
+    
