907ed3f037d114a570cc8c404b8837bca8e5c9ab
unknown <sangaypenjor289@gmail.com>
2024-05-15 14:22:55 +0530

    skiping

---
+++ skipping.py
@@ -0,0 +1,11 @@
+for i in range (1,9):
+    if i == 3:
+        print("skipping 3, in the inner loop")
+        continue
+
+    if i == 7:
+        print("reached 7, breaking the outer loop")
+        break
+
+    print(i)
+ 
