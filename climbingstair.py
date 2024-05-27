23de6168731524e868ea1d927d9768677c339969
unknown <sangaypenjor289@gmail.com>
2024-05-27 15:10:45 +0530

    climbling stair

---
+++ climbingstair.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def climeStairs (self, n): 
+        memo = {}
+        return self.ways(n, memo)
+    def ways(self, n , memo):
+        if n in memo:
+            return memo[n]
+        if n==1:
+            return 1
+        elif n==2:
+            return 2
+        memo[n] = self.ways(n - 1, memo) + self.ways(n - 2, memo)
+        return memo[n]
